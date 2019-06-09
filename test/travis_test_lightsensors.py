610557c07a5ae8398a46a71d0c03ea4448a460c1
Yasushi Oshima <oosmyss@gmail.com>
2019-06-09 15:34:37 +0000

    Debug test

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -43,7 +43,7 @@ class LightsensorTest(unittest.TestCase):
         c_prev = self.count
         time.sleep(3)
         self.assertTrue((self.count < (c_prev + 4)), "freq does not change")
-        self.assertTrue((self.count == c_prev + 4), "subscriber is stopped")
+        self.assertTrue((self.count == c_prev), "subscriber is stopped")
 
 if __name__ == '__main__':
     time.sleep(3)
