745fe859ecfb2ea9fb4614599d31e0f782597a03
Yasushi Oshima <oosmyss@gmail.com>
2019-06-09 11:07:06 +0000

    Debug travis test

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -31,7 +31,7 @@ class LightsensorTest(unittest.TestCase):
         rospy.set_param('lightsensors_freq', 10)
         time.sleep(2)
         with open("/dev/rtlightsensor0", "w") as f:
-            f.write("-1 0 123 4321")
+            f.write("-1, 0, 123, 4321")
         
         time.sleep(3)
         self.assertFalse((self.count == 0), "cannot subscribe the topic")
